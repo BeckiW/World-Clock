a600b30e989ad29a990eadff23db2ed7f1c25162
Becki Wordsworth <r.a.wordsworth@gmail.com>
2018-10-05 23:42:48 +0200

    Clock stuff
    Added some styling and a load of buttons

--- code/src/index.js
+++ code/src/index.js
@@ -1,18 +1,107 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import Moment from "moment-timezone"
 import "./index.css"
 
-class Clock extends React.Component {
 
+class Header extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+        <header>
+          <h1>World Time Clock</h1>
+        </header>
       </div>
     )
   }
-
 }
 
-ReactDOM.render(<Clock date={new Date()} />, document.getElementById("root"))
+class Clock extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "Stockholm",
+      timezone: "Europe/Stockholm",
+      moment: new Moment()
+    };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+     clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      moment: new Moment()
+    });
+  }
+
+  pauseClock = () => {
+    clearInterval(this.timerID)
+  }
+
+  startClock = () => {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    )
+  }
+
+  changeZoneClick = (sender) => {
+    let buttonText = sender.target.innerText;
+
+    if ( buttonText == "STOCKHOLM" ) {
+      this.setState({
+        title: "Stockholm",
+        timezone: "Europe/Stockholm"
+      });
+
+    } else if ( buttonText == "BRAZIL" ) {
+      this.setState({
+        title: "Brazil (Sao Paulo)",
+        timezone: "America/Sao_Paulo"
+      });
+
+    } else if ( buttonText == "SINGAPORE" ) {
+      this.setState({
+        title: "Singapore",
+        timezone: "Asia/Singapore"
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className="clock">
+        <h2>{this.state.title}</h2>
+        <h3 className="time">{this.state.moment.tz(this.state.timezone).format('HH:mm:ss')}</h3>
+        <div className="buttonGrid">
+          <button onClick={this.changeZoneClick}>STOCKHOLM</button>
+          <button onClick={this.changeZoneClick}>BRAZIL</button>
+          <button onClick={this.changeZoneClick}>SINGAPORE</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+ReactDOM.render(
+  <div>
+    <Header />
+    <div className="clock-grid">
+      <Clock />
+      <Clock />
+    </div>
+  </div>
+  ,
+  document.getElementById('root')
+);
